61d08fffd3fdaaf07e05543aff3946ee6301b11e
Andres Salvatierra <andressalvatierra11@gmail.com>
2019-05-25 17:26:45 -0300

    Punto 1

--- SO2_TP3/andres/punto1/informacion.c
+++ SO2_TP3/andres/punto1/informacion.c
@@ -5,49 +5,49 @@
 #include <time.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include <sys/sysinfo.h>
 
 #define TAM 1024
 
-struct embebido		//Informacion referida al embebido
+/*struct embebido		//Informacion referida al embebido
 {
-	char uptime[20];	
 	char cpu[20];
 	char memoria[20];
-};
+};*/
 
 
-static struct embebido raspi;
+//static struct embebido raspi;
 static struct tm *tlocal;
-static int minuto,segundo;
 
 void info_embebido();
-void dif_hora();
 
 int main(int argc, char *argv[])
 {	
     time_t tiempo = time(0);	//Obtengo la hora de comienzo de ejecucion del proceso
 	tlocal= localtime(&tiempo);
-	minuto=tlocal->tm_min;
-	segundo=tlocal->tm_sec;	
-
-	printf("Content-Type: text/plain;charset=us-ascii\n\n");
     
+	printf("\n Fecha y hora actual: \n %s \n", ctime(&tiempo));
+
 	info_embebido();
 
-    printf("Uptime: %s\n",raspi.uptime);
-    printf("Memoria en uso: %s\n",raspi.memoria);
-    printf("Consumo de CPU: %s\n",raspi.cpu);
+    //printf("Memoria en uso: %s\n",raspi.memoria);
+    //printf("Consumo de CPU: %s\n",raspi.cpu);
 
 }
 
 void info_embebido()
 {
     char parameter[TAM]={0},pid[10]={0},buffer[TAM]={0};
-	FILE *fp;
-	memset( raspi.uptime, '\0', sizeof(raspi.uptime));
-	memset( raspi.memoria, '\0', sizeof(raspi.memoria));
-	memset( raspi.cpu, '\0', sizeof(raspi.cpu));
-    
+    struct sysinfo s_info;
+    int error= sysinfo(&s_info);
+    if(error!=0){
+    printf("error=%d\n",error);
+    }
+    FILE *fp;
+	//memset( raspi.memoria, '\0', sizeof(raspi.memoria));
+	//memset( raspi.cpu, '\0', sizeof(raspi.cpu));
+	
+	printf("\n Uptime: %ld segundos \n",s_info.uptime);
 	//system("rm ./info");
 	strcpy(parameter,"ps -Ao vsize,pcpu,pid | grep ");
 	sprintf(pid,"%i",getpid()); //Obtengo el pid para filtrar el ps
@@ -65,14 +65,17 @@ void info_embebido()
 		int indice=0;
 		while(token != NULL){
 			if(indice==0)
-				strcpy(raspi.memoria,token);
+				//strcpy(raspi.memoria,token);
+				printf("Memoria en uso: %s \n",token);
 			if(indice==1)
-				strcpy(raspi.cpu,token);
+				//strcpy(raspi.cpu,token);
+				printf("Cpu en uso: %s \n",token);
 			indice++;	
 			token=strtok(NULL," ");
 		}
 	}
 
+	printf("\n");
 	printf("Informacion del procesador: \n");
 
 	FILE *cpuinfoFile;
@@ -97,36 +100,6 @@ void info_embebido()
 		if((modelCpu = strstr(lineRead, "model name"))!= NULL)
 			printf("%s \n",modelCpu);
 
-	}	
-
-	dif_hora();
+	}		
 }
 
-/**
- * @brief Funcion encargada de obtener el tiempo que lleva ejecutandose el proceso satelite en el sistema.
-  */
-void dif_hora()
-{	
-
-	char hora[24]={0},min[5]={0},seg[5]={0},actual[30]={0};
-	time_t tiempo_actual = time(0);
-	struct tm *tactual= localtime(&tiempo_actual);
-
-	int minutos= abs(tactual->tm_min -minuto);
-	int segundos=tactual->tm_sec;
-	
-	if(segundos<segundo)
-	{
-		minutos=minutos-1;
-		segundos=segundos+60;
-	}
-
-	sprintf(min,"%i",minutos);
-	sprintf(seg,"%i",segundos-segundo);
-	strcpy(hora,min);
-	strcat(hora,":");
-	strcat(hora,seg);
-	strcpy(raspi.uptime,hora);
-
-	printf("\n Fecha y hora actual: \n %s \n", ctime(&tiempo_actual));
-}
