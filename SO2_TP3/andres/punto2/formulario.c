d9eed51c527e680e4bb90a19c81e7679b72b0677
andressalvatierrra <andressalvatierra11@gmail.com>
2019-06-01 12:39:40 -0300

    actualizacion de make y punto 3

--- SO2_TP3/andres/punto2/formulario.c
+++ SO2_TP3/andres/punto2/formulario.c
@@ -18,13 +18,12 @@ strcat(anio,inputParse);
 strcat(dia,inputParse);
 
 char *year=strtok(dia,"&");
-printf("El anio es %s \n",year);
+printf("El listado solicitado del anio %s",year);
 
 char *day=strtok(NULL," ");
 day=strtok(day,"=");
 day=strtok(NULL," ");
-printf("El dia es %s \n",day);
-
+printf(" y dia %s es:\n",day);
 
 char path[100]={0};
 strcpy(path,"aws s3 --no-sign-request ls --recursive noaa-goes16/ABI-L2-CMIPF/");
@@ -33,35 +32,36 @@ strcat(path,"/");
 strcat(path,day);
 strcat(path,"/");
 strcat(path," | grep M3C13");
-strcat(path," >> ./informacion");
+system(path);
+//strcat(path," >> ./informacion"); PONER
 //printf("%s \n",path);
 
 //system("aws s3 --no-sign-request ls --recursive noaa-goes16/ABI-L2-CMIPF/2017/320/ > ./info");
 
-system("rm ./informacion");
-system("touch ./informacion");
-system(path);
+//system("rm ./informacion");
+//system("touch ./informacion");
+//system(path);
 
-	FILE *fp;
-	int tam_file_send, n;
-	char buffer[1024];
+//	FILE *fp;
+//	int tam_file_send, n;
+//	char buffer[1024];
 
-	fp=fopen("./informacion","r");
-	if (fp == NULL)
-	{
-		printf("Error al abrir el file\n");
-		exit(1);
-	}
+//	fp=fopen("./informacion","r");
+//	if (fp == NULL)
+//	{
+//		printf("Error al abrir el file\n");
+//		exit(1);
+//	}
 
-	while(!feof(fp))	//Si no llegue al final del archivo
-	{	
-		memset(buffer,'\0',sizeof(buffer));//Quiero mandar mas de un paquete lo reinicializo
-	        fread(buffer, 1, sizeof(buffer), fp);
-		printf("%s \n",buffer);
-	}
-	fclose(fp);	
+//	while(!feof(fp))	//Si no llegue al final del archivo
+//	{	
+//		memset(buffer,'\0',sizeof(buffer));//Quiero mandar mas de un paquete lo reinicializo
+//	        fread(buffer, 1, sizeof(buffer), fp);
+//		printf("%s \n",buffer);
+//	}
+//	fclose(fp);	
 
 return 0;
-}
 
+}
 
